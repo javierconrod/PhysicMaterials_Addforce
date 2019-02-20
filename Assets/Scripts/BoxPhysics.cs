e5229c511201e4775de407391fbcc5bc6586e4fe
Javier Contreras <javierin_kl@hotmail.com>
2019-02-20 08:52:42 -0700

    Initial commit

---
+++ Assets/Scripts/BoxPhysics.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxPhysics : MonoBehaviour
+{
+    Rigidbody2D rb2d;
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.AddForce(Vector2.up*500);
+    }
+}
